86af8105b46e03eda8f8d73dc9d6f5765e63adee
Daniel Chapdelaine <daniel.chapdelaine@vitalsource.com>
2021-11-22 13:21:33 -0600

    add article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,35 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.all()
+    @articles = Article.all
+  end
 
+  def new
+    # we must define the @article instance variable at new else we cannot ask if there are errors in our template
+    # ref: <% if @article.errors.any? %>
+    @article = Article.new
+  end
+
+  def edit 
+
+  end
+
+  def create
+    # require/permit allow our application to whitelist title and description for use in our controller
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created scucessfully."
+      #redirect_to article_path(@article)
+      redirect_to @article
+    else 
+      # re-render the page and show errors
+      render 'new'
+    end
+  end
+
+  def update
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # resources gives you a ton of out-of-the-box methods for Model
   # you can choose to add specific methods using `only`
   # without `only`, all methods will be available in you routes. test with `rails routes --expanded`
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
