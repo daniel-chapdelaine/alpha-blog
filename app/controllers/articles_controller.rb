008f21bf7d8e377da4bf6cce634861469c8b54e6
Daniel Chapdelaine <daniel.chapdelaine@vitalsource.com>
2021-11-22 15:52:00 -0600

    DRY code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -14,13 +14,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit 
-    @article = Article.find(params[:id])
+  def edit
   end
 
   def create
     # require/permit allow our application to whitelist title and description for use in our controller
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created sucessfully."
@@ -33,9 +32,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated sucessfully."
       redirect_to @article
     else
@@ -44,9 +41,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end  
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  
 end
