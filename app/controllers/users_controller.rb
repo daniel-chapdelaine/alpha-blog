1b91bc93d141d02cb816d8db6aeed423370c9cce
Daniel Chapdelaine <daniel.chapdelaine@vitalsource.com>
2021-11-29 12:30:40 -0600

    admin permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +37,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles deleted."
+    redirect_to articles_path
+  end
+ 
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -47,7 +55,7 @@ class UsersController < ApplicationController
   end  
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin? 
       flash[:alert] = "You can only edit your own account"
       redirect_to @user
     end
